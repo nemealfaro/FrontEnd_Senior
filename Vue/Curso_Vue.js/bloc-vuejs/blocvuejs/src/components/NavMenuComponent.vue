ae0409644909ca28adcf71099e9585230feb9f1c
nemalfaro29 <nalfaroperez29@gmail.com>
2024-06-04 16:29:32 -0600

    Create home page content

--- Vue/Curso_Vue.js/bloc-vuejs/blocvuejs/src/components/NavMenuComponent.vue
+++ Vue/Curso_Vue.js/bloc-vuejs/blocvuejs/src/components/NavMenuComponent.vue
@@ -2,7 +2,7 @@
    <nav class="navbar navbar-expand-lg bg-primary">
   <div class="container justify-content-evenly">
           <RouterLink class="nav-link active" aria-current="page" to="/"><span class="material-icons-round">home</span></RouterLink>
-          <RouterLink class="nav-link" to="/"><span class="material-icons-round">info</span></RouterLink>
+          <RouterLink class="nav-link" aria-current="page" to="/about"><span class="material-icons-round">info</span></RouterLink>
   </div>
 </nav>
 </template>
--- Vue/Curso_Vue.js/bloc-vuejs/blocvuejs/src/views/HomeView.vue
+++ Vue/Curso_Vue.js/bloc-vuejs/blocvuejs/src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup>
-
-</script>
-
 <template>
   <main>
+    <div class="home">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="img-container">
+            <div class="d-flex">
+              <img src="../assets/logo.svg" alt="logo vue">
+            </div>
+            <div>
+              <br>
+              <h2>Mi blog de Vue.js, Comparte, Comenta y Aprende sobre el desarrollo de Vue</h2>
+            </div>
+          </div>
+        </div>
 
+        <div class="col-lg-6">
+          
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+
+<script setup>
+  
+</script>
